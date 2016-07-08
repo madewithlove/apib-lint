f130f877bd7f9a2f17751e05eba7346686d6c547
Maxime Fabre <ehtnam6@gmail.com>
2016-07-08 11:54:54 +0200

    Linting

--- src/lint.js
+++ src/lint.js
@@ -1,30 +1,31 @@
 import fs from 'fs';
-import process from 'process';
 import colors from 'colors';
 import protagonist from 'protagonist';
 
 export default function (file) {
-    const source = fs.readFileSync(file).toString();
+    fs.readFile(file, (errors, source) => {
+        source = source.toString();
 
-    protagonist.parse(source, {type: 'ast'}, function (error, result) {
-        if (error) {
-            console.error(error);
-        }
+        protagonist.parse(source, {type: 'ast'}, (error, result) => {
+            if (error) {
+                console.error(error);
+            }
 
-        if (result.warnings.length) {
-            result.warnings.forEach(warning => {
-                const {message, location} = warning;
-                const presenter = "\n>> ";
+            if (result.warnings.length) {
+                result.warnings.forEach(warning => {
+                    const {message, location} = warning;
+                    const presenter = '\n>> ';
 
-                // Get code snippet
-                const {index, length} = location[0];
-                let code = source.slice(index, index + length).trim();
-                code = presenter + code.replace(/\n/g, presenter);
+                    // Get code snippet
+                    const {index, length} = location[0];
+                    let code = source.slice(index, index + length).trim();
+                    code = presenter + code.replace(/\n/g, presenter);
 
-                console.log(`${colors.red('WARN:')} ${message}${colors.grey(code)}\n`);
-            });
+                    console.log(`${colors.red('WARN:')} ${message}${colors.grey(code)}\n`);
+                });
 
-            process.exit(1);
-        }
+                throw new Error(`${result.warnings.length} warnings found in APIB file`);
+            }
+        });
     });
 }
